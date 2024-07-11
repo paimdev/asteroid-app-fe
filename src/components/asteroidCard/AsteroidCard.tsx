8a7e34701a3f86605dc68f07bd9c2466dbd9d235
paimdev <lucas@paim.dev>
2024-07-11 15:50:20 +0100

    feat: add favorite button and functionality

--- src/components/asteroidCard/AsteroidCard.tsx
+++ src/components/asteroidCard/AsteroidCard.tsx
@@ -1,9 +1,33 @@
-const AsteroidCard = (asteroid: any) => {
+import axios from "axios";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+const AsteroidCard = ({ asteroid }: any) => {
+  async function addFavorite(id: string, name: string, url: string) {
+    const response = await axios.post(`http://localhost:3000/favorites`, {
+      id: id,
+      name: name,
+      url: url,
+    });
+  }
+
   return (
-    <>
-      <li>
-        <h3>{asteroid.name}</h3>
-        <p>ID: {asteroid.id}</p>
+    <Card>
+      <CardHeader
+        className="cursor-pointer"
+        onClick={() => {
+          location.href = asteroid.nasa_jpl_url;
+        }}
+      >
+        <CardTitle>{asteroid.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
         <p>Absolute Magnitude: {asteroid.absolute_magnitude_h}</p>
         <p>
           Is Potentially Hazardous:{" "}
@@ -18,17 +42,18 @@ const AsteroidCard = (asteroid: any) => {
           {asteroid.estimated_diameter.kilometers.estimated_diameter_min} -{" "}
           {asteroid.estimated_diameter.kilometers.estimated_diameter_max}
         </p>
-        <p>
-          <a
-            href={asteroid.nasa_jpl_url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            NASA JPL Link
-          </a>
-        </p>
-      </li>
-    </>
+      </CardContent>
+      <CardFooter>
+        <Button
+          variant="secondary"
+          onClick={() =>
+            addFavorite(asteroid.id, asteroid.name, asteroid.nasa_jpl_url)
+          }
+        >
+          Add to favorites
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 export default AsteroidCard;
--- src/components/asteroidList/AsteroidList.tsx
+++ src/components/asteroidList/AsteroidList.tsx
@@ -1,11 +1,4 @@
-import { Button } from "../ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import AsteroidCard from "../asteroidCard/AsteroidCard";
 
 const AsteroidList = ({ asteroidData }: any) => {
   const sortAsteroidsByName = (asteroids: any[]) => {
@@ -21,42 +14,7 @@ const AsteroidList = ({ asteroidData }: any) => {
           <h2>{date}</h2>
           <ul>
             {sortAsteroidsByName(asteroidData[date]).map((asteroid: any) => (
-              <Card key={asteroid.id}>
-                <CardHeader
-                  className="cursor-pointer"
-                  onClick={() => {
-                    location.href = asteroid.nasa_jpl_url;
-                  }}
-                >
-                  <CardTitle>{asteroid.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>Absolute Magnitude: {asteroid.absolute_magnitude_h}</p>
-                  <p>
-                    Is Potentially Hazardous:{" "}
-                    {asteroid.is_potentially_hazardous_asteroid ? "Yes" : "No"}
-                  </p>
-                  <p>
-                    Close Approach Date:{" "}
-                    {asteroid.close_approach_data[0].close_approach_date_full}
-                  </p>
-                  <p>
-                    Estimated Diameter (min/max km):{" "}
-                    {
-                      asteroid.estimated_diameter.kilometers
-                        .estimated_diameter_min
-                    }{" "}
-                    -{" "}
-                    {
-                      asteroid.estimated_diameter.kilometers
-                        .estimated_diameter_max
-                    }
-                  </p>
-                </CardContent>
-                <CardFooter>
-                  <Button variant="secondary">Add to favorites</Button>
-                </CardFooter>
-              </Card>
+              <AsteroidCard asteroid={asteroid} key={asteroid.id} />
             ))}
           </ul>
         </div>
